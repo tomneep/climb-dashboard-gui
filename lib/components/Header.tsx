aa0b4b6680da46abb91c7e516480065fc114c001
tombch <t.brier@outlook.com>
2025-11-20 12:32:18 +0000

    Added brand, fixed collapse behaviour, made links open in new tab

--- lib/components/Header.tsx
+++ lib/components/Header.tsx
@@ -5,12 +5,32 @@ import Navbar from "react-bootstrap/Navbar";
 export function Header() {
   return (
     <Navbar expand="md" className="bg-dark" variant="dark">
-      <Container>
+      <Container fluid>
+        <Navbar.Brand title="CLIMB Dashboard">CLIMB Dashboard</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="https://docs.climb.ac.uk">Documentation</Nav.Link>
-            <Nav.Link href="mailto:support@climb.ac.uk">Support</Nav.Link>
-            <Nav.Link href="https://bryn.climb.ac.uk">Bryn</Nav.Link>
+            <Nav.Link
+              href="https://docs.climb.ac.uk"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Documentation
+            </Nav.Link>
+            <Nav.Link
+              href="mailto:support@climb.ac.uk"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Support
+            </Nav.Link>
+            <Nav.Link
+              href="https://bryn.climb.ac.uk"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Bryn
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
