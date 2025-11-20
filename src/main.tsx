0f54d937e848221bbddf09777eb1d8eec832f1fe
tombch <t.brier@outlook.com>
2025-11-20 12:41:47 +0000

    Fixed type error for dummy volumes handler, and added formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Main from "../lib/App.tsx";
-import { userInfoHandler, resourcesHandler, volumesHandler } from "./handlers.tsx";
+import {
+  userInfoHandler,
+  resourcesHandler,
+  volumesHandler,
+} from "./handlers.tsx";
 
 ReactDOM.render(
   <React.StrictMode>
     <Main
-  userInfoHandler={userInfoHandler}
-  resourcesHandler={resourcesHandler}
-  volumesHandler={volumesHandler}
+      userInfoHandler={userInfoHandler}
+      resourcesHandler={resourcesHandler}
+      volumesHandler={volumesHandler}
     />
   </React.StrictMode>,
   document.getElementById("root")
